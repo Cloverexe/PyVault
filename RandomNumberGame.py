60b3eb593c6d321db20f6558e699c00e5b74af5a
Cloverexe <96324493+Cloverexe@users.noreply.github.com>
2021-12-17 23:13:56 -0600

    Create RandomNumberGame.py

---
+++ RandomNumberGame.py
@@ -0,0 +1,11 @@
+# From what I understand right now this just imports the random function so a random whatever can be used.
+# In this case, random.randint
+import random
+# This functions makes the computer select and print a random number between 1 and 100 and prints the 
+# value selected.
+def random_number():
+    rand = random.randint(1, 100)
+    print(rand)
+    
+num = 3    
+random_number
