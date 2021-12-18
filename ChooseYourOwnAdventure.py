b0b5b20b2348a93c7da8cca107eb9f2519cfa31e
Cloverexe <96324493+Cloverexe@users.noreply.github.com>
2021-12-18 10:31:57 -0600

    Update and rename ChooseYourOwnAdventur` to ChooseYourOwnAdventure.py

---
+++ ChooseYourOwnAdventure.py
@@ -0,0 +1,19 @@
+def user_input():
+    user_input = input(print("Choose an option: "))
+print("...")
+print("Where...")
+print("Where am I?")
+print("You wake up in the middle of a large field.")
+print("Your ears ring at a painful pitch")
+print("")
+print("You stand up, noticing the scraped, dented armor you wore.")
+print("Broken rocks littered around you.")
+print("Swords, arrows, bows, and spears surround you.")
+print("Along with the bodies of their wielders.")
+print("As your ears adjust and the ringing dies down you start ")
+print("hearing the sounds of yelling and metal clashing.")
+user_input
+print("a. Pick up a weapon and walk to the noise")
+print("b. Walk to the noise")
+print("c. Run away towards the river.")
+`     `               `
